5ea209b78c4d57b9613be3cdc77060035d0b8d4a
2mm4 <melaniesendeavors@gmail.com>
2025-11-16 13:50:22 -0500

    merged changes

--- calculator.py
+++ calculator.py
@@ -1,5 +1,4 @@
 import math
-import math
 
 def square_root(a):
         if a < 0:
@@ -10,52 +9,25 @@ def square_root(a):
 def hypotenuse(a, b):
     return math.hypot(a, b)
 
-def add(a, b): 
-    return a + b
-
 def add(a, b):
     return a + b
 
 def subtract(a, b):
     return a - b
 
-def sub(a, b):
-    return a - b
-
-
-def multiply(a, b):
-    return a * b
-
 def mul(a, b):
     return a * b
-
-def divide(a, b):
-    if a == 0:
-        raise ZeroDivisionError
-    else:
-        return b / a
 
 def div(a, b):
     if a == 0:
         raise ZeroDivisionError("Cannot divide by zero (argument 'a' was 0).")
     return b / a
 
-
-
 def logarithm(a, b):
     if a <= 0 or b <= 0:
         raise ValueError
     else:
         return math.log(a, b)
-
-def log(a, b):
-    if a <= 0 or a == 1:
-         raise ValueError("Logarithm base 'a' must be positive and not equal to 1.")
-    if b <= 0:
-         raise ValueError("Logarithm argument 'b' must be positive.")
-
-def exponent(a, b):
-    return a ** b
 
 def exp(a, b):
     return a ** b
